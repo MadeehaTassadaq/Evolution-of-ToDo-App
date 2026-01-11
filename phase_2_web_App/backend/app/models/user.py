2305a209713997ce6a3edc4c65e840b3dc8e8463
MadeehaTassadaq <madeehatassadaq@outlook.com>
2026-01-11 20:58:08 +0500

    cors handles

--- phase_2_web_App/backend/app/models/user.py
+++ phase_2_web_App/backend/app/models/user.py
@@ -1,10 +1,14 @@
 from sqlmodel import Field, SQLModel
 from uuid import UUID, uuid4
-from datetime import datetime
+from datetime import datetime, timezone
+
+def utc_now():
+    return datetime.now(timezone.utc)
+
 
 class User(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     email: str = Field(unique=True, index=True)
     password_hash: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=utc_now)
+    updated_at: datetime = Field(default_factory=utc_now)
