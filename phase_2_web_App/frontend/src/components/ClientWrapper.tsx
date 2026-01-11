8741b775c466c38fb47be1ab488a9be67a5f048d
MadeehaTassadaq <madeehatassadaq@outlook.com>
2026-01-11 13:30:53 +0500

    more_files

--- phase_2_web_App/frontend/src/components/ClientWrapper.tsx
+++ phase_2_web_App/frontend/src/components/ClientWrapper.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from '../context/AuthContext';
 import { ReactNode } from 'react';
 
 export default function ClientWrapper({ children }: { children: ReactNode }) {
